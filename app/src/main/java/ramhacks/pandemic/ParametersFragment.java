8c83a21c9c3db2d8ae62bf624c3e4fb91f5ca548
Aaron Throckmorton <rickthrockmorton@gmail.com>
2015-09-12 19:03:57 -0400

    added mFragmentActionCallback to ParametersFragment

--- app/src/main/java/ramhacks/pandemic/ParametersFragment.java
+++ app/src/main/java/ramhacks/pandemic/ParametersFragment.java
@@ -6,19 +6,22 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 import butterknife.Bind;
 import butterknife.OnClick;
+import ramhacks.pandemic.Interfaces.OnFragmentActionListener;
 
 
 public class ParametersFragment extends Fragment {
 
-    //private OnFragmentInteractionListener mListener;
     //@Bind(R.id.btn_heatmap) public Button mBtnHeatMap;
+    private OnFragmentActionListener mFragmentActionCallback;
 
     public static ParametersFragment newInstance() {
         ParametersFragment fragment = new ParametersFragment();
@@ -35,8 +38,9 @@ public class ParametersFragment extends Fragment {
 
     }
 
-    // @OnClick(R.id.btn_heatmap)
+    @OnClick(R.id.btn_heatmap)
     public void segueToHeatActivity(){
+        Log.d("ayyy", "lmao");
         Intent intent = new Intent(getActivity(), HeatMapActivity.class);
         getActivity().startActivity(intent);
     }
@@ -53,6 +57,12 @@ public class ParametersFragment extends Fragment {
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
+        try {
+            mFragmentActionCallback = (OnFragmentActionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentActionListener");
+        }
 
     }
 
