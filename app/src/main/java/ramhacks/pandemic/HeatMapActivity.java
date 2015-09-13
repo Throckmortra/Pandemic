1aabc37c8100bdbd83d3df66b97dc7ca0065e863
Aaron Throckmorton <rickthrockmorton@gmail.com>
2015-09-12 22:25:12 -0400

    asdf

--- app/src/main/java/ramhacks/pandemic/HeatMapActivity.java
+++ app/src/main/java/ramhacks/pandemic/HeatMapActivity.java
@@ -2,21 +2,45 @@ package ramhacks.pandemic;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+import com.reimaginebanking.api.java.Models.Geocode;
+import com.reimaginebanking.api.java.Models.Merchant;
+import com.reimaginebanking.api.java.NessieClient;
+import com.reimaginebanking.api.java.NessieException;
+import com.reimaginebanking.api.java.NessieResultsListener;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 public class HeatMapActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private HeatmapTileProvider mProvider;
+    private TileOverlay mOverlay;
+    private TileOverlay mOverlayz;
+    private ArrayList<LatLng> mLocations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heat_map);
         setUpMapIfNeeded();
+        mLocations = new ArrayList<>();
+
+        getLocations();
+
+
     }
 
     @Override
@@ -52,6 +76,76 @@ public class HeatMapActivity extends FragmentActivity {
             }
         }
     }
+
+    private void createHeatMap(List<LatLng> locations){
+
+        mProvider = new HeatmapTileProvider.Builder()
+                .data(locations)
+                .build();
+        // Add a tile overlay to the map, using the heat map tile provider.
+        mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+    }
+
+    private void getLocations(){
+        NessieClient nessieClient = NessieClient.getInstance();
+        nessieClient.setAPIKey("3a942e124e29f5d830e92f88808e096b");
+
+        HashMap body = new HashMap<>(3);
+        body.put("lat", 33);
+        body.put("lng", -77);
+        body.put("rad", 100);
+
+
+        nessieClient.getMerchants(new NessieResultsListener() {
+            @Override
+            public void onSuccess(Object result, NessieException e) {
+                if (e == null) {
+                    //There is no error, do whatever you need here.
+                    // Cast the result object to the type that you are requesting and you are good to go
+                    if(result == null){
+                        return;
+                    }
+
+                    ArrayList<Merchant> customers = (ArrayList<Merchant>) result;
+
+                    for (int i = 0; i < customers.size(); i++) {
+                        LatLng temp;
+                        Merchant merchant = customers.get(i);
+
+                        if(merchant==null){
+                            continue;
+                        }
+
+                        Geocode geocode = merchant.getGeocode();
+
+                        if(geocode == null){
+                            continue;
+                        }
+                        double lat = geocode.getLat();
+                        double lng = geocode.getLng();
+
+                        if(lat != 0.0 && lng != 0.0){
+                            temp = new LatLng(lat,lng);
+                            mLocations.add(temp);
+                        }
+                        //LatLng temp = new LatLng(latz,lngz);
+//                        if(temp != null){
+//                            mLocations.add(i, temp);
+//                        }
+
+                        Log.d("ayyy", customers.get(i).getName() + "   " + lat + "  " + lng + "      #"+ i);
+                    }
+                    createHeatMap(mLocations);
+
+                } else {
+                    //There was an error. Handle it here
+                    Log.e("Error", e.toString());
+                }
+            }
+        });
+
+    }
+
 
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
--- app/src/main/java/ramhacks/pandemic/MainActivity.java
+++ app/src/main/java/ramhacks/pandemic/MainActivity.java
@@ -7,29 +7,66 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.FrameLayout;
+import android.widget.Spinner;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.reimaginebanking.api.java.Models.Merchant;
+import com.reimaginebanking.api.java.NessieClient;
+import com.reimaginebanking.api.java.NessieException;
+import com.reimaginebanking.api.java.NessieResultsListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import ramhacks.pandemic.Interfaces.OnFragmentActionListener;
 
-public class MainActivity extends AppCompatActivity implements OnFragmentActionListener {
+public class MainActivity extends AppCompatActivity implements OnFragmentActionListener,
+        AdapterView.OnItemSelectedListener {
 
-//    @Bind(R.id.fragment_container)
-//    public FrameLayout fragmentContainer;
+    @Bind(R.id.radius_spinner)
+    public Spinner mSpinner;
+    private List<LatLng> mLocations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        setSpinner();
 
 //        NessieClient nessieClient = NessieClient.getInstance();
-//        nessieClient.setAPIKey("ea4cc6f6e2755426e5e060913d3d1ef0");
-
-
-        //replaceWithParameterFragment();
+//        nessieClient.setAPIKey("3a942e124e29f5d830e92f88808e096b");
+//
+//        nessieClient.getMerchants(new NessieResultsListener() {
+//            @Override
+//            public void onSuccess(Object result, NessieException e) {
+//                if(e == null){
+//                    //There is no error, do whatever you need here.
+//                    // Cast the result object to the type that you are requesting and you are good to go
+//                    ArrayList<Merchant> customers = (ArrayList<Merchant>) result;
+//
+//                    for (int i = 0; i < customers.size(); i++) {
+//                        float lat = customers.get(i).getGeocode().getLat();
+//                        float lng = customers.get(i).getGeocode().getLng();
+//                        mLocations.add(new LatLng(lat, lng));
+//                    }
+//
+//                } else {
+//                    //There was an error. Handle it here
+//                    Log.e("Error", e.toString());
+//                }
+//            }
+//        });
+//
+//
+//        //replaceWithParameterFragment();
     }
 
     @Override
@@ -44,6 +81,25 @@ public class MainActivity extends AppCompatActivity implements OnFragmentActionL
         Log.d("ayy", "lmao");
         Intent intent = new Intent(this, HeatMapActivity.class);
         this.startActivity(intent);
+    }
+
+    private void setSpinner(){
+        String[] radiusNums = new String[5];
+        radiusNums[0] = "10 miles";
+        radiusNums[1] = "25 miles";
+        radiusNums[2] = "100 miles";
+        radiusNums[3] = "500 miles";
+        radiusNums[4] = "1000 miles";
+
+
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_dropdown_item, radiusNums);
+        spinnerAdapter.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+
+        mSpinner.setAdapter(spinnerAdapter);
+        mSpinner.setSelection(5, false);
+        //mSpinner.setOnItemClickListener(this);
     }
 
     @Override
@@ -85,4 +141,13 @@ public class MainActivity extends AppCompatActivity implements OnFragmentActionL
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Log.d("ayy", "lmao");
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
