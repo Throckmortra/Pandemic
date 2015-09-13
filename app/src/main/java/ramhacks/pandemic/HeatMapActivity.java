83fcfbfdeb8b29949b887269426f388b9daea79a
Aaron Throckmorton <rickthrockmorton@gmail.com>
2015-09-13 01:32:19 -0400

    slowly I descend into madness while I eat this chewy granola bar

--- app/src/main/java/ramhacks/pandemic/HeatMapActivity.java
+++ app/src/main/java/ramhacks/pandemic/HeatMapActivity.java
@@ -11,11 +11,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
 import com.google.maps.android.heatmaps.HeatmapTileProvider;
+import com.google.maps.android.heatmaps.WeightedLatLng;
 import com.reimaginebanking.api.java.NessieClient;
 import com.reimaginebanking.api.java.NessieException;
 import com.reimaginebanking.api.java.NessieResultsListener;
 import com.reimaginebanking.api.java.models.Geocode;
 import com.reimaginebanking.api.java.models.Merchant;
+import com.reimaginebanking.api.java.models.Purchase;
 
 
 import java.util.ArrayList;
@@ -23,13 +25,19 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
+import ramhacks.pandemic.Models.MerchantWeight;
+
 public class HeatMapActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private HeatmapTileProvider mProvider;
     private TileOverlay mOverlay;
     private TileOverlay mOverlayz;
-    private ArrayList<LatLng> mLocations;
+    private ArrayList<WeightedLatLng> mLocations;
+    private ArrayList<Purchase> mPurchases;
+    private ArrayList<Merchant> mMerchants;
+    private ArrayList<MerchantWeight> mMerchantWeight;
+    private NessieClient nessieClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +45,10 @@ public class HeatMapActivity extends FragmentActivity {
         setContentView(R.layout.activity_heat_map);
         setUpMapIfNeeded();
         mLocations = new ArrayList<>();
-
+        mPurchases = new ArrayList<>();
+        mMerchants = new ArrayList<>();
+        mMerchantWeight = new ArrayList<>();
+        nessieClient = NessieClient.getInstance();
         getLocations();
 
 
@@ -77,10 +88,10 @@ public class HeatMapActivity extends FragmentActivity {
         }
     }
 
-    private void createHeatMap(List<LatLng> locations){
+    private void createHeatMap(List<WeightedLatLng> locations){
 
         mProvider = new HeatmapTileProvider.Builder()
-                .data(locations)
+                .weightedData(locations)
                 .build();
         // Add a tile overlay to the map, using the heat map tile provider.
         mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
@@ -91,47 +102,43 @@ public class HeatMapActivity extends FragmentActivity {
         nessieClient.setAPIKey("3a942e124e29f5d830e92f88808e096b");
 
 
-        nessieClient.getMerchants("37","-77","1000" ,new NessieResultsListener() {
+        nessieClient.getMerchants("37", "-77", "1000", new NessieResultsListener() {
             @Override
             public void onSuccess(Object result, NessieException e) {
                 if (e == null) {
                     //There is no error, do whatever you need here.
                     // Cast the result object to the type that you are requesting and you are good to go
-                    if(result == null){
+                    if (result == null) {
                         return;
                     }
 
                     ArrayList<Merchant> customers = (ArrayList<Merchant>) result;
-
+                    mMerchants = customers;
                     for (int i = 0; i < customers.size(); i++) {
                         LatLng temp;
                         Merchant merchant = customers.get(i);
 
-                        if(merchant==null){
+                        if (merchant == null) {
                             continue;
                         }
 
                         Geocode geocode = merchant.getGeocode();
 
-                        if(geocode == null){
+                        if (geocode == null) {
                             continue;
                         }
                         double lat = geocode.getLat();
                         double lng = geocode.getLng();
 
-                        if(lat != 0.0 && lng != 0.0){
-                            temp = new LatLng(lat,lng);
-                            mLocations.add(temp);
+                        if (lat != 0.0 && lng != 0.0) {
+                            temp = new LatLng(lat, lng);
+                            //mLocations.add(temp);
                         }
-                        //LatLng temp = new LatLng(latz,lngz);
-//                        if(temp != null){
-//                            mLocations.add(i, temp);
-//                        }
 
-                        Log.d("ayyy", customers.get(i).getName() + "   " + lat + "  " + lng + "      #"+ i);
+                        Log.d("ayyy", customers.get(i).getName() + "   " + lat + "  " + lng + "      #" + i);
                     }
 
-                    //getPurchases();
+                    getPurchases();
                     //createHeatMap(mLocations);
 
                 } else {
@@ -143,7 +150,67 @@ public class HeatMapActivity extends FragmentActivity {
 
     }
 
+    private void getPurchases(){
+        nessieClient.getPurchases("55e94a6cf8d8770528e6169b", new NessieResultsListener() {
+            @Override
+            public void onSuccess(Object result, NessieException e) {
+                if (e == null) {
+                    //There is no error, do whatever you need here.
+                    // Cast the result object to the type that you are requesting and you are good to go
+                    if (result == null) {
+                        return;
+                    }
 
+                    mPurchases = (ArrayList<Purchase>) result;
+
+                    computeWeight();
+                } else {
+                    //There was an error. Handle it here
+                    Log.e("Error", e.toString());
+                }
+            }
+        });
+    }
+
+    private void computeWeight(){
+
+        for (int x = 0; x < mMerchants.size(); x++) {
+
+            Merchant merchant = mMerchants.get(x);
+            if (merchant == null) {
+                continue;
+            }
+
+            String id = merchant.get_id();
+            String name = merchant.getName();
+
+            Geocode geocode = merchant.getGeocode();
+
+            if (geocode == null) {
+                continue;
+            }
+
+            int weight = 1;
+            mMerchantWeight.add(new MerchantWeight(id, name, geocode, weight));
+        }
+
+        for(int i = 0; i < mMerchantWeight.size(); i++){
+            for (int n = 0; n < mPurchases.size(); n++) {
+                if(mMerchantWeight.get(i).getId().equals(mPurchases.get(n).getMerchant_id())){
+                    mMerchantWeight.get(i).incrementWeight();
+                }
+            }
+        }
+
+        for (int z = 0; z < mMerchantWeight.size(); z++) {
+            double lat = mMerchantWeight.get(z).getGeocode().getLat();
+            double lng = mMerchantWeight.get(z).getGeocode().getLng();
+            double weight = mMerchantWeight.get(z).getWeight();
+            mLocations.add(new WeightedLatLng(new LatLng(lat,lng), weight));
+        }
+
+        createHeatMap(mLocations);
+    }
     /**
      * This is where we can add markers or lines, add listeners or move the camera. In this case, we
      * just add a marker near Africa.
---
+++ app/src/main/java/ramhacks/pandemic/Models/MerchantWeight.java
@@ -0,0 +1,46 @@
+package ramhacks.pandemic.Models;
+
+import com.reimaginebanking.api.java.models.Address;
+import com.reimaginebanking.api.java.models.Geocode;
+
+/**
+ * Created by aaron on 9/12/15.
+ */
+public class MerchantWeight {
+    private int weight;
+    private Geocode geocode;
+    private String id;
+    private String name;
+
+    public MerchantWeight(String id, String name, Geocode geocode, int weight) {
+        this.weight = weight;
+        this.geocode = geocode;
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public Geocode getGeocode() {
+        return geocode;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void incrementWeight(){
+        weight = weight++;
+    }
+
+}
